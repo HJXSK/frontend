0da46fa2077e6e70a4908f1c3705db35cff2f0f3
Hao <wenhaoy.0428@gmail.com>
2024-07-07 21:31:40 +0800

    feat: enable user to upload user avatars
    1. The user profile page reflects the user profile
    
    Signed-off-by: Hao <wenhaoy.0428@gmail.com>

--- src/app/home/settings/user/index.tsx
+++ src/app/home/settings/user/index.tsx
@@ -1,8 +1,8 @@
-import {View, Button, StyleSheet, TextInput, Text} from 'react-native';
+import {Button, StyleSheet, TextInput, Text} from 'react-native';
 import {SettingItem, SettingSection} from '@/components/settings/item';
 import Avatar from '@/components/settings/avatar';
 import ScrollPage from '@/components/page';
-import {useState, useMemo, useLayoutEffect} from 'react';
+import {useState, useMemo, useLayoutEffect, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {
   selectUserProfile,
@@ -10,6 +10,9 @@ import {
 } from '@/redux/features/user/userProfileSlice';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {AppStackParamList} from '@/app';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import * as ImagePicker from 'expo-image-picker';
+import {downloadFileAsync, uploadFileAsync} from '@/util/firebase';
 
 type UserType = Record<string, any>;
 
@@ -34,7 +37,6 @@ const UserSettingPage: React.FC<UserSettingPageProps> = ({
   const [changes, isEdited] = useMemo(() => {
     const result = Object.keys(newUserInfo).reduce((acc, key) => {
       if (newUserInfo[key] !== userProfile[key]) {
-        console.log('Key: ', key, newUserInfo[key], userProfile[key]);
         acc[key] = newUserInfo[key];
       }
       return acc;
@@ -57,18 +59,48 @@ const UserSettingPage: React.FC<UserSettingPageProps> = ({
   const saveUser = async () => {
     try {
       // Dispatch an action to update the user profile in Redux
-
+      if (changes.gs_user_avatar) {
+        const path = await uploadFileAsync(changes.gs_user_avatar, 'image');
+        changes.gs_user_avatar = path;
+        setNewUserInfo({...newUserInfo, gs_user_avatar: path});
+      }
       dispatch(update_profile(changes));
     } catch (error) {
       console.error('Error updating document: ', error);
     }
   };
 
+  useEffect(() => {
+    downloadFileAsync(userProfile.gs_user_avatar!).then(({url}) => {
+      setImage(url);
+    });
+  }, [userProfile.gs_user_avatar]);
+
+  const [image, setImage] = useState<string | null>();
+
+  const updateAvatar = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      // https://stackoverflow.com/questions/70528896/react-native-expo-image-picker-upload-image-to-firebase-storage-v9-crash
+      quality: 0.5, // as well as using uploadBytesResumable
+    });
+
+    if (!result.canceled) {
+      result.assets.forEach(async asset => {
+        setImage(asset.uri);
+        setNewUserInfo({...newUserInfo, gs_user_avatar: asset.uri});
+      });
+    }
+  };
+
   return (
     <ScrollPage>
-      <View style={styles.avatarWrapper}>
-        <Avatar width={100} height={100} />
-      </View>
+      <TouchableOpacity style={styles.avatarWrapper} onPress={updateAvatar}>
+        <Avatar width={100} height={100} src={image} />
+      </TouchableOpacity>
       <SettingSection>
         <SettingItem title="Name">
           <TextInput
